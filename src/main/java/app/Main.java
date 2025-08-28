0cfd93d459b515f73cd3f86e9969374598228c50
DavidK97 <david.700@live.dk>
2025-08-28 13:35:08 +0200

    Initial commit

---
+++ src/main/java/app/Main.java
@@ -0,0 +1,18 @@
+package app;
+
+import app.config.HibernateConfig;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+        EntityManager em = emf.createEntityManager();
+
+
+
+
+        em.close();
+        emf.close();
+    }
+}
