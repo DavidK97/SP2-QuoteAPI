cd53bb14731fe6d7984b20aad6e38a0b747c7daf
William1372 <william.ludwig.oster.dano@gmail.com>
2025-10-23 23:54:02 +0200

    Endpoints, requests m.m. tilføjet

--- src/main/java/app/routes/Routes.java
+++ src/main/java/app/routes/Routes.java
@@ -7,11 +7,13 @@ import static io.javalin.apibuilder.ApiBuilder.path;
 public class Routes {
     private final QuoteRoutes quoteRoutes = new QuoteRoutes();
     private final CategoryRoutes categoryRoutes = new CategoryRoutes();
+    private final AuthorRoutes authorRoutes = new AuthorRoutes();
 
     public EndpointGroup getRoutes() {
         return () -> {
             path("/quotes", quoteRoutes.getRoutes());
             path("/categories", categoryRoutes.getRoutes());
+            path("/authors", authorRoutes.getRoutes());
             //path("/rooms", roomRoute.getRoutes());
         };
     }
