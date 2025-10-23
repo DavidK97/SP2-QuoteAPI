d12095cf0872a63a55f0848faa3a740b4b5a4495
William1372 <william.ludwig.oster.dano@gmail.com>
2025-10-23 23:34:11 +0200

    Tests og populator

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,6 +3,7 @@ package app;
 import app.config.ApplicationConfig;
 import app.config.HibernateConfig;
 import app.config.QuotePopulator;
+import app.populators.AuthorPopulator;
 import io.javalin.Javalin;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
@@ -12,7 +13,8 @@ public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
 
-        QuotePopulator.populate(emf);
+       // QuotePopulator.populate(emf);
+       // AuthorPopulator.populate(emf);
 
         Javalin app = ApplicationConfig.startServer(7076);
         
