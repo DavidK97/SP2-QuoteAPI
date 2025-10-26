9cf414661ac5d9af66fa78a2cb7384a0ad64d779
DavidK97 <david.700@live.dk>
2025-10-26 15:53:15 +0100

    removed tests classes for deployment

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -16,9 +16,7 @@ public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
 
-        // QuotePopulator.populate(emf);
-
-
+        QuotePopulator.populate(emf);
 
         Javalin app = ApplicationConfig.startServer(7070);
     }
